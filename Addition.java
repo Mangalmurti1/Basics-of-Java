858939eebe3c97256b4db92142a5e7504e801735
Mangalmurti1 <rokademahesh1000@gmail.com>
2024-06-02 10:27:27 +0530

    Heu i have added new project in eclipse

---
+++ Addition.java
@@ -0,0 +1,12 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a=1,b=20,c;
+     c=a+b;
+     System.out.println("Additon of a and b is = "+c);
+     
+	}
+
+}
